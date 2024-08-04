0617abc2c5ca0e0c226dbab63694d0c1594aa7a2
Sumant <sumantrane52@gmail.com>
2024-08-04 12:39:37 +0530

    (chore): added service logic

--- e-commerce-app/src/app/app.module.ts
+++ e-commerce-app/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent, CheckoutComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
   ],
--- e-commerce-app/src/app/services/product.service.ts
+++ e-commerce-app/src/app/services/product.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Observable, shareReplay } from 'rxjs';
+import { Product } from '../interfaces/product.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private apiUrl = 'https://fakestoreapi.com/products';
+  constructor(private _http: HttpClient) {}
 
-  constructor() { }
+  //get all products
+  getProducts(): Observable<Product[]> {
+    return this._http.get<Product[]>(this.apiUrl).pipe(shareReplay(1));
+  }
+
+  //get single product
+  getProduct(id: number): Observable<Product> {
+    return this._http.get<Product>(`${this.apiUrl}/${id}`).pipe(shareReplay(1));
+  }
+
+  //add product
+  addProduct(product: Product): Observable<Product> {
+    return this._http.post<Product>(this.apiUrl, product);
+  }
+
+  //update product
+  updateProduct(id: number, product: Product): Observable<Product> {
+    return this._http.put<Product>(`${this.apiUrl}/${id}`, product);
+  }
+
+  //delete product
+  deleteProduct(id: number): Observable<Product> {
+    return this._http.delete<Product>(`${this.apiUrl}/${id}`);
+  }
 }
